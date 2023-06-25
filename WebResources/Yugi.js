49640d903ec8ff1d8557b2e98e2a8b76958d4436
YA <YA-androidapp@users.noreply.github.com>
2023-06-25 20:52:07 +0900

    Update Yugi.js

--- WebResources/Yugi.js
+++ WebResources/Yugi.js
@@ -16,31 +16,39 @@ var Yugi = window.Yugi || {};
         /// window.setTimeout(function () { formContext.ui.clearFormNotification(myUniqueId); }, 5000);
     }
 
-    // Code to run in the column OnChange event 
+    // Code to run in the column OnChange event
     this.attributeOnChange = function (executionContext) {
         var formContext = executionContext.getFormContext();
 
         // Automatically set some column values if the account name contains "Contoso"
         const fileValue = formContext.getAttribute("ya_file").getValue();
-        console.log("fileValue", fileValue)
+        if (fileValue) {
+            console.log("fileValue", fileValue)
+            fetch(fileValue.fileUrl)
+                .then(res => {
+                    console.log("res", res)
+                    console.log("blob", res.blob())
 
-        /// fetch(fileValue.fileUrl)
-        /// .then(res => res.arrayBuffer())
-        /// .then(buffer => {
-        ///         const mp3tag = new MP3Tag(buffer)
-        ///         mp3tag.read()
-        ///         console.log(mp3tag.tags)
-        /// })
-        /// .catch(err => console.error(err.message));
+                    var reader = new FileReader()
+                    reader.onload = function () {
+                        const mp3tag = new MP3Tag(reader.result)
+                        mp3tag.read()
+                        console.log(mp3tag.tags)
+                    };
+                    reader.readAsArrayBuffer(res.blob())
+                })
+            /// .catch(err => console.error(err.message));
 
-        /// if (accountName.toLowerCase().search("contoso") != -1) {
-        ///     formContext.getAttribute("websiteurl").setValue("https://www.contoso.com");
-        ///     formContext.getAttribute("telephone1").setValue("425-555-0100");
-        ///     formContext.getAttribute("description").setValue("Website URL, Phone and Description set using custom script.");
-        /// }
+            /// if (accountName.toLowerCase().search("contoso") != -1) {
+            ///     formContext.getAttribute("websiteurl").setValue("https://www.contoso.com");
+            ///     formContext.getAttribute("telephone1").setValue("425-555-0100");
+            ///     formContext.getAttribute("description").setValue("Website URL, Phone and Description set using custom script.");
+            /// }
+
+        }
     }
 
-    // Code to run in the form OnSave event 
+    // Code to run in the form OnSave event
     this.formOnSave = function () {
         // Display an alert dialog
         /// Xrm.Navigation.openAlertDialog({ text: "Record saved." });
